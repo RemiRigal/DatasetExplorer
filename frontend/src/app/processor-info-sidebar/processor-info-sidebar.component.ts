6dcd2493ad7ad90ffc7860b909b04bec489b635b
Rémi Rigal <remi.rigal@orange.com>
2020-04-28 16:25:10 +0200

    Add processor info sidebar

---
+++ frontend/src/app/processor-info-sidebar/processor-info-sidebar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-processor-info-sidebar',
+  templateUrl: './processor-info-sidebar.component.html',
+  styleUrls: ['./processor-info-sidebar.component.css']
+})
+export class ProcessorInfoSidebarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
